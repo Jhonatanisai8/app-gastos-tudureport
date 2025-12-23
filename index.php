2e51b801ab145924aa81c669e178a03a790d31ac
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-22 22:51:32 -0500

    agregacion del index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require 'php/IniciarSesion.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<?php
+include "./inc/Head.php"
+?>
+
+<body> <?php
+
+        if (!isset($_GET['vista']) || $_GET['vista'] == "") {
+            $_GET['vista'] = "login";
+        }
+        if (
+            is_file("./views/" . $_GET['vista'] . ".php")
+            && $_GET['vista'] != "login"
+            && $_GET['vista'] != "registro"
+            && $_GET['vista'] != "404"
+        ) {
+            include './inc/Navbar.php';
+            include "./views/" . $_GET['vista'] . ".php";
+            include './inc/Script.php';
+        } else {
+            if ($_GET['vista'] == "login") {
+                include './views/login.php';
+            }
+            if ($_GET['vista'] == "registro") {
+                include './views/registro.php';
+            }
+        }
+        ?>
+</body>
+
+</html>
