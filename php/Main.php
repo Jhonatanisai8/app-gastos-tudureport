d000d6cbe11678a1e8dd1756ec53a822d449d467
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-22 21:35:40 -0500

    conexion con la base de datos

---
+++ php/Main.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+
+// Cargar las variables de entorno
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+function conexion()
+{
+    try {
+        $host = $_ENV['DB_HOST'];
+        $port = $_ENV['DB_PORT'];
+        $db   = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASS'];
+
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+
+        $PDO = new PDO($dsn, $user, $pass);
+        $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $PDO;
+    } catch (PDOException $e) {
+        die("Error de conexión: " . $e->getMessage());
+    }
+}
