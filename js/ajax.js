f21d00141c3af8f433ab6f38017486c725042f13
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-22 23:09:09 -0500

    agregando ajax al app para el envio de form

---
+++ js/ajax.js
@@ -0,0 +1,36 @@
+console.log("Hola mundo desde ajax ");
+const formularios_ajax = document.querySelectorAll(".formularioAjax");
+
+function enviar_formulario_ajax(e) {
+  e.preventDefault();
+  console.log("Ejecutando funcion enviar_formulario_ajax");
+
+  let data = new FormData(this);
+  let method = this.getAttribute("method");
+  let action = this.getAttribute("action");
+
+  let headers = new Headers();
+  let config = {
+    method: method,
+    headers: headers,
+    mode: "cors",
+    cache: "no-cache",
+    body: data,
+  };
+
+  fetch(action, config)
+    .then((response) => response.text())
+    .then((response) => {
+      let container = document.querySelector(".form-rest");
+      container.innerHTML = response;
+
+      // limpiamos los campos
+      if (!response.includes("notification is-danger")) {
+        this.reset();
+      }
+    });
+}
+
+formularios_ajax.forEach((formulario) => {
+  formulario.addEventListener("submit", enviar_formulario_ajax);
+});
