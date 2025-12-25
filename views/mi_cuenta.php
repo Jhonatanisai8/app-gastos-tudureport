7628b54627553d561a399950e5fc5bf7c80ce820
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-25 16:38:30 -0500

    personalizaion del icono para mostrar la inicial del usuario

--- views/mi_cuenta.php
+++ views/mi_cuenta.php
@@ -4,7 +4,7 @@ $id = $_SESSION['id_usuario']; ?>
 <div class="account-section-container">
     <div class="profile-header">
         <div class="avatar-wrapper">
-            <div class="avatar-circle"><?= $nombreUsuario.chop(0,1) ?></div> <a href="editar_perfil.php" class="btn-edit-profile"> <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
+            <div class="avatar-circle"><?= $nombreUsuario[0] ?></div> <a href="editar_perfil.php" class="btn-edit-profile"> <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5">
                     <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                     <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                 </svg> Editar Perfil </a>
