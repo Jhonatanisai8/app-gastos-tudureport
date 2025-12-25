0607cee3247aaa26ce427f5ec83555119840d386
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-25 15:45:18 -0500

    css

--- inc/Head.php
+++ inc/Head.php
@@ -4,8 +4,8 @@
     <title>APP DEMO GASTOS</title>
     <link rel="stylesheet" href="css/404.css">
     <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="css/registro.css">
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/registro_gastos.css">
+    <link rel="stylesheet" href="css/registro.css">
 </head>
